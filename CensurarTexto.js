ebc55c7858e8e14c2bd82fbda62285682ecf142d
Manuel <manuelmao94@gmail.com>
2020-05-09 16:03:39 -0300

    agregue codigo

--- CensurarTexto.js
+++ CensurarTexto.js
@@ -1,2 +1,30 @@
+function obtenerExpresionCensurada(expresion){
+	let expresionCensurada = ''
+
+	for (let i = 0; i < expresion.length; i++) {
+		expresionCensurada = expresionCensurada + '*'
+	}
+	return expresionCensurada
+}
+// gente
+// =>
+// expresion
+
+function censurarTexto(frase, expresionesCensuradas){
+	let fraseCensurada = frase
+
+	for (let i = 0; i < expresionesCensuradas.length; i++) {
+		const expresion = expresionesCensuradas[i]
+
+		if (frase.includes(expresion)) {
+			fraseCensurada = fraseCensurada.replace(expresion, obtenerExpresionCensurada(expresion))
+
+		}
+	}
+	return fraseCensurada
+}
+
+console.log(censurarTexto('no veo el momento en que la cuarentena se termine cuarentena', ['cuarentena']))
+
 
 
